6d8074a0c421cd37e872a6fe89c5e5bf16454356
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-08-11 04:18:57 -0400

    gave up on google maps the pods were just not loading and xcode kept messing up decided to go with Apple Maps the Geolocation is kind of tricky but I will figure that out in the morning because I have a map

--- components/MyMaps.js
+++ components/MyMaps.js
@@ -1,238 +1,51 @@
-import React from 'react'
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'
+import React, { Component } from 'react'
+import MapView from 'react-native-maps'
+import { StyleSheet, Dimensions, View } from 'react-native'
+// import Geolocation from 'react-native-geolocation-service';
 
-const mapStyle = [
-  {
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#ebe3cd'
-      }
-    ]
-  },
-  {
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#523735'
-      }
-    ]
-  },
-  {
-    elementType: 'labels.text.stroke',
-    stylers: [
-      {
-        color: '#f5f1e6'
-      }
-    ]
-  },
-  {
-    featureType: 'administrative',
-    elementType: 'geometry.stroke',
-    stylers: [
-      {
-        color: '#c9b2a6'
-      }
-    ]
-  },
-  {
-    featureType: 'administrative.land_parcel',
-    elementType: 'geometry.stroke',
-    stylers: [
-      {
-        color: '#dcd2be'
-      }
-    ]
-  },
-  {
-    featureType: 'administrative.land_parcel',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#ae9e90'
-      }
-    ]
-  },
-  {
-    featureType: 'landscape.natural',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#dfd2ae'
-      }
-    ]
-  },
-  {
-    featureType: 'poi',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#dfd2ae'
-      }
-    ]
-  },
-  {
-    featureType: 'poi',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#93817c'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'geometry.fill',
-    stylers: [
-      {
-        color: '#a5b076'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#447530'
-      }
-    ]
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#f5f1e6'
-      }
-    ]
-  },
-  {
-    featureType: 'road.arterial',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#fdfcf8'
-      }
-    ]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#f8c967'
-      }
-    ]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry.stroke',
-    stylers: [
-      {
-        color: '#e9bc62'
-      }
-    ]
-  },
-  {
-    featureType: 'road.highway.controlled_access',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#e98d58'
-      }
-    ]
-  },
-  {
-    featureType: 'road.highway.controlled_access',
-    elementType: 'geometry.stroke',
-    stylers: [
-      {
-        color: '#db8555'
-      }
-    ]
-  },
-  {
-    featureType: 'road.local',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#806b63'
-      }
-    ]
-  },
-  {
-    featureType: 'transit.line',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#dfd2ae'
-      }
-    ]
-  },
-  {
-    featureType: 'transit.line',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#8f7d77'
-      }
-    ]
-  },
-  {
-    featureType: 'transit.line',
-    elementType: 'labels.text.stroke',
-    stylers: [
-      {
-        color: '#ebe3cd'
-      }
-    ]
-  },
-  {
-    featureType: 'transit.station',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#dfd2ae'
-      }
-    ]
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry.fill',
-    stylers: [
-      {
-        color: '#b9d3c2'
-      }
-    ]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#92998d'
-      }
-    ]
-  }
-]
 
-export default class App extends React.Component {
-
-  render() {
+let {height, width} = Dimensions.get('window')
+const styles = StyleSheet.create({
+ container: {
+   height: height,
+   width: width,
+ },
+ map: {
+   ...StyleSheet.absoluteFillObject,
+ },
+})
+export default class MyMaps extends Component{
+//   componentDidMount() {
+//     let hasLocationPermission
+//     // Instead of navigator.geolocation, just use Geolocation.
+//     if (hasLocationPermission) {
+//         Geolocation.getCurrentPosition(
+//             (position) => {
+//                 console.log(position);
+//             },
+//             (error) => {
+//                 // See error code charts below.
+//                 console.log(error.code, error.message);
+//             },
+//             { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+//         );
+//     }
+// }
+  render(){
     return (
-      <MapView
-        provide={PROVIDER_GOOGLE}
-        customMapStyle={mapStyle}
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCxzDBk-4CNB_XCHCLT-I2XsjeXmsVE-DQ&callback=initMap"
-        region={{
-          latitude: 42.882004,
-          longitude: 74.582748,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-        showsUserLocation={true}
-      />
-    );
+      <View style={styles.container}>
+        <MapView
+           style={styles.map}
+           initialRegion={{
+             latitude: 37.78825,
+             longitude: -122.4324,
+             latitudeDelta: 0.0922,
+             longitudeDelta: 0.0421,
+           }}
+           />
+      </View>
+      )
+    }
   }
-}
+ 
+
--- ios/Pods/Target Support Files/Pods-MyReactNativeTests/Pods-MyReactNativeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyReactNativeTests : NSObject
-@end
-@implementation PodsDummy_Pods_MyReactNativeTests
-@end
