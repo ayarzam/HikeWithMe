98ddceea4458da3a1659fae162952413dcef9446
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-08-11 11:19:00 -0400

    added functionality for geolocation next up amimating the map to followmy location

--- components/MyMaps.js
+++ components/MyMaps.js
@@ -14,34 +14,45 @@ const styles = StyleSheet.create({
    ...StyleSheet.absoluteFillObject,
  },
 })
+
+const props = {}
 export default class MyMaps extends Component{
-//   componentDidMount() {
-//     let hasLocationPermission
-//     // Instead of navigator.geolocation, just use Geolocation.
-//     if (hasLocationPermission) {
-//         Geolocation.getCurrentPosition(
-//             (position) => {
-//                 console.log(position);
-//             },
-//             (error) => {
-//                 // See error code charts below.
-//                 console.log(error.code, error.message);
-//             },
-//             { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-//         );
-//     }
-// }
+  constructor(props){
+    super(props)
+      this.state = {
+        latitude: 0,
+        longitude:0
+      }
+    
+  }
+  componentDidMount() {
+    let hasLocationPermission
+    // Instead of navigator.geolocation, just use Geolocation.
+    if (hasLocationPermission) {
+        navigator.eolocation.getCurrentPosition(
+            (position) => {
+                console.log(position);
+            },
+            (error) => {
+                // See error code charts below.
+                console.log(error.code, error.message);
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+    }
+}
   render(){
     return (
       <View style={styles.container}>
         <MapView
            style={styles.map}
            initialRegion={{
-             latitude: 37.78825,
-             longitude: -122.4324,
-             latitudeDelta: 0.0922,
-             longitudeDelta: 0.0421,
+             latitude: 40.730610,
+             longitude: -73.935242,
+             latitudeDelta: 1,
+             longitudeDelta: 1,
            }}
+           showsUserLocation={true}
            />
       </View>
       )
