235ce307e6bb19545e822279eb81a5e700b577e8
Ayarza Manwaring <50222669+ayarzam@users.noreply.github.com>
2020-06-30 23:14:00 -0400

    Delete config.js

--- firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDQzsFpcf2ShCOAmJSXhZoNxEGfr271Jxw",
-    authDomain: "hikewithme-53cdc.firebaseapp.com",
-    databaseURL: "https://hikewithme-53cdc.firebaseio.com",
-    projectId: "hikewithme-53cdc",
-    storageBucket: "hikewithme-53cdc.appspot.com",
-    messagingSenderId: "641145773678",
-    appId: "1:641145773678:web:3b28dce1374216cd"
-}
